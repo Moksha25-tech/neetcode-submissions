ff4c94c97f8366a307cdce4aeaf2907b57079b19
Moksha Choksi <moksha.choksi@students.iiit.ac.in>
2026-06-04 23:02:18 +0530

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, jump = 0;
+        if(n == 1){
+            return 0;
+        }
+        while(i<n-2){
+            if(i+nums[i] >= n-1){
+                jump++;
+                return jump;
+            }
+            if(nums[i+1] > nums[i + nums[i]]){
+                jump++;
+                i++;
+                if(i>=n-1){
+                    return jump;
+                }
+            }
+            else{
+                i=i+nums[i];
+                jump++;
+                if(i>=n-1){
+                    return jump;
+                }
+            }
+        }
+    }
+};
