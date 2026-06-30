7eacf603fc31c0b9184e474233a7d403b35f2256
Moksha Choksi <moksha.choksi@students.iiit.ac.in>
2026-06-30 21:10:30 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        vector<int> ans;
+        ans.push_back(nums[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for (int i = 0; i < ans.size(); i++) {
+            nums[i] = ans[i];
+        }
+
+        return ans.size();
+    }
+};
