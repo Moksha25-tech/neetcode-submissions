03701c56a20adda8a5ac80f512dd9e4ba40496dd
Moksha Choksi <moksha.choksi@students.iiit.ac.in>
2026-07-08 17:21:37 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string curr = "";
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (curr == "" || curr == ".") {
+                    // Ignore empty components and "."
+                } 
+                else if (curr == "..") {
+                    if (!st.empty())
+                        st.pop();
+                } 
+                else {
+                    st.push(curr);
+                }
+                curr = "";
+            } 
+            else {
+                curr += path[i];
+            }
+        }
+
+        if (st.empty())
+            return "/";
+
+        vector<string> dirs;
+        while (!st.empty()) {
+            dirs.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(dirs.begin(), dirs.end());
+
+        string ans = "";
+        for (string &dir : dirs) {
+            ans += "/" + dir;
+        }
+
+        return ans;
+    }
+};
